4bec3310bde894f4e6d9f2912bf337d72b30a70d
Rick Holmes <rickh877@gmail.com>
2019-11-05 20:44:33 -0500

    Added serial debug code for testing

--- cell_phone_sw/cell_phone_sw.ino
+++ cell_phone_sw/cell_phone_sw.ino
@@ -46,26 +46,38 @@ GSM gsmAccess;
 GSM_SMS sms;
 
 void sendTxt(String txtMsg){
-    if(numMessages < 100){
+      Serial.print(txtMsg);
+  
+/*    if(numMessages < 100){
        sms.beginSMS(senderNumber);
        sms.print(txtMsg);
        sms.endSMS();
        numMessages++;
     }    
+*/    
 }
 
 void setup() {
   txtMessage.reserve(200);
   // connection state
   bool connected = false;
-
+  Serial.begin(9600);
+  while (!Serial){
+    ;
+  }
+  Serial.println("serial port started");
   // Start GSM shield
   // If your SIM has PIN, pass it as a parameter of begin() in quotes
   while (!connected) {
-    if (gsmAccess.begin(PINNUMBER) == GSM_READY) {
-        }
-    delay(1000);
+    if (gsmAccess.begin(PINNUMBER) == GSM_READY){ 
+        connected = true;
+        Serial.println("GSM started");
     }
+    else{
+       Serial.println("Not connected");
+       delay(1000);
+    }
+  }  
   pinMode(RELAY, OUTPUT);
   pinMode(BUTTON, INPUT);
   digitalWrite(RELAY, LOW);
@@ -75,18 +87,19 @@ void loop() {
   int c;
   int i;
   char t;
+  Serial.println("in loop");
   // put your main code here, to run repeatedly:
   // check to see if the button is pressed
-  if (digitalRead(BUTTON) == 0){
-    if(FLAG == 0){
-       FLAG = 1;
-       digitalWrite(RELAY, HIGH);
+  if (digitalRead(BUTTON) == 1){
+    if(FLAG == 1){
+       FLAG = 0;
+       digitalWrite(RELAY, LOW);
        outMessage = txtMessage + txtMessage3 + txtMessage2;
        sendTxt(outMessage);
        }
     else{
-       FLAG = 0;
-       digitalWrite(RELAY, LOW);
+       FLAG = 1;
+       digitalWrite(RELAY, HIGH);
        outMessage = txtMessage + txtMessage3 + txtMessage1;
        sendTxt(outMessage);
       } 
@@ -105,13 +118,15 @@ void loop() {
    while ((c = sms.read()) != -1){
        txtMessage += (char)c;
    }
-       
-   if( txtMessage.length() == 7){
+   Serial.println(txtMessage);
+   if( txtMessage.length() == 6){
       if (txtMessage.substring(0,4) != "9536"){
          sendTxt(txtMessage4);
-      }
+         Serial.print("command is ");
+         Serial.println(txtMessage.charAt(5));
+      }   
       else{
-          t = txtMessage[5];
+          t = txtMessage.charAt(5);
           if(t == 'n'){
              FLAG = 1;
              digitalWrite(RELAY, HIGH);    
